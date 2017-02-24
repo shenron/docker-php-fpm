da9f44ad80f291c70aa15ff5f3f418901aabe9a6
Thomas CASTELLY <shenron@tcy.io>
2017-02-24 10:09:44 +0100

    fix don t rewrite config each time at startup

--- php_fpm/conf/entrypoint.sh
+++ php_fpm/conf/entrypoint.sh
@@ -1,27 +1,39 @@
 #!/usr/bin/env bash
 
+extFolder=$(php-config --extension-dir)
+xdebugPath=/usr/local/etc/php/conf.d/
+
+# allow debug in DEV
+# update config file only if needed
 if [ "$ENV" == "DEV" ]; then
-  extFolder=$(php-config --extension-dir)
-  echo "zend_extension=$extFolder/xdebug.so" >> /usr/local/etc/php/php.ini
-
-  xdebugPath=/usr/local/etc/php/conf.d/
-  touch $xdebugPath/xdebug.ini
-
-  # On linux it's possible to try to connect to the client
-  # On MacOS X, the IP of host have to be set
-  if [ -z "$IP_HOST" ]; then 
-    echo xdebug.remote_connect_back=1 >> $xdebugPath/xdebug.ini
+  if grep -q "xdebug" /usr/local/etc/php/php.ini; then
+    echo "[LOG] debug config already apply"
   else
-    echo xdebug.remote_connect_back=0 >> $xdebugPath/xdebug.ini
-    echo "xdebug.remote_host=$IP_HOST" >> $xdebugPath/xdebug.ini
+    echo "zend_extension=$extFolder/xdebug.so" >> /usr/local/etc/php/php.ini
+
+    echo "" > xdebugPath/xdebug.ini
+
+    # On linux it's possible to try to connect to the client
+    # On MacOS X, the IP of host have to be set
+    if [ -z "$IP_HOST" ]; then 
+      echo xdebug.remote_connect_back=1 >> $xdebugPath/xdebug.ini
+    else
+      echo xdebug.remote_connect_back=0 >> $xdebugPath/xdebug.ini
+      echo "xdebug.remote_host=$IP_HOST" >> $xdebugPath/xdebug.ini
+    fi
+
+    echo xdebug.remote_enable=1 >> $xdebugPath/xdebug.ini
+    echo xdebug.remote_autostart=0 >> $xdebugPath/xdebug.ini
+    echo xdebug.remote_port=9000 >> $xdebugPath/xdebug.ini
+    echo xdebug.remote_log=/tmp/php-xdebug.log >> $xdebugPath/xdebug.ini
+
+    sed -i "s/opcache.revalidate_freq=60/opcache.revalidate_freq=0/" /usr/local/etc/php/php.ini
   fi
-
-  echo xdebug.remote_enable=1 >> $xdebugPath/xdebug.ini
-  echo xdebug.remote_autostart=0 >> $xdebugPath/xdebug.ini
-  echo xdebug.remote_port=9000 >> $xdebugPath/xdebug.ini
-  echo xdebug.remote_log=/tmp/php-xdebug.log >> $xdebugPath/xdebug.ini
-
-  sed -i "s/opcache.revalidate_freq=60/opcache.revalidate_freq=0/" /usr/local/etc/php/php.ini
+else 
+  if [ -f $xdebugPath/xdebug.ini ]; then
+    rm $xdebugPath/xdebug.ini
+    sed -i "/xdebug/d" /usr/local/etc/php/php.ini
+  fi
 fi
 
 exec "$@"
