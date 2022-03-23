a79f7b0e7770cb5c3f2f228c0e4159a0a72c12e5
Thomas CASTELLY <thomas@tcy.io>
2022-03-23 18:47:32 +0100

    add oci, in progress ...

--- php_fpm_oracle_client/install_oracle_driver.sh
+++ php_fpm_oracle_client/install_oracle_driver.sh
@@ -5,15 +5,15 @@ cd /tmp
 
 if [ `arch` == "x86_64" ]; then
   echo "download and install for x86_64"
-  unzip ./instantclient-basiclite-linux.x64-19.3.0.0.0dbru.zip -d ./unzip && \
-    unzip ./instantclient-sdk-linux.x64-19.3.0.0.0dbru.zip -d ./unzip && \
+  unzip ./instantclient-basiclite-linux.x64-19.3.0.0.0dbru.zip -d /usr/local && \
+    unzip ./instantclient-sdk-linux.x64-19.3.0.0.0dbru.zip -d /usr/local && \
     rm ./*.zip &&\
-    mv ./unzip/instantclient_19_3 ./unzip/instantclient
+    ln -s /usr/local/instantclient_19_3 /usr/local/instantclient
 else
   echo "download and install for arm"
-  unzip ./instantclient-basiclite-linux.arm64-19.10.0.0.0dbru.zip -d ./unzip && \
-    unzip ./instantclient-sdk-linux.arm64-19.10.0.0.0dbru.zip -d ./unzip && \
+  unzip ./instantclient-basiclite-linux.arm64-19.10.0.0.0dbru.zip -d /usr/local && \
+    unzip ./instantclient-sdk-linux.arm64-19.10.0.0.0dbru.zip -d /usr/local && \
     rm ./*.zip &&\
-    mv ./unzip/instantclient_19_10 ./unzip/instantclient
+    ln -s /usr/local/instantclient_19_10 /usr/local/instantclient
 fi
 
