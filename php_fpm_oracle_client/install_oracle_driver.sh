ab285fd3204ec0082922b6181e165ceb14625f8e
Thomas CASTELLY <thomas@tcy.io>
2022-03-23 23:32:05 +0100

    enable multistage to reduce size of image

--- php_fpm_oracle_client/install_oracle_driver.sh
+++ php_fpm_oracle_client/install_oracle_driver.sh
@@ -8,12 +8,12 @@ if [ `arch` == "x86_64" ]; then
   unzip ./instantclient-basiclite-linux.x64-19.3.0.0.0dbru.zip -d /usr/local && \
     unzip ./instantclient-sdk-linux.x64-19.3.0.0.0dbru.zip -d /usr/local && \
     rm ./*.zip &&\
-    ln -s /usr/local/instantclient_19_3 /usr/local/instantclient
+    mv /usr/local/instantclient_19_3 /usr/local/instantclient
 else
   echo "download and install for arm"
   unzip ./instantclient-basiclite-linux.arm64-19.10.0.0.0dbru.zip -d /usr/local && \
     unzip ./instantclient-sdk-linux.arm64-19.10.0.0.0dbru.zip -d /usr/local && \
     rm ./*.zip &&\
-    ln -s /usr/local/instantclient_19_10 /usr/local/instantclient
+    mv /usr/local/instantclient_19_10 /usr/local/instantclient
 fi
 
